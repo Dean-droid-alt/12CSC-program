7fa446631c8eec23e388d4b208b3ea4b89d1c02a
Dean-droid-alt <23023@students.mrgs.school.nz>
2026-05-01 12:34:56 +1200

    Starting trial 2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from PIL import Image, ImageTk
-from tkinter import Tk, Canvas, NW
 
 root = tk.Tk()
 
@@ -11,7 +10,7 @@ label.image = photo
 label.pack()
 
 play_button = tk.PhotoImage(file="Image_Gallery/play_button.png")
-btn = tk.Button(root, image=play_button, command=lambda: print("Clicked!"))
+btn = tk.Button(root, image=play_button, cursor="hand2", command=lambda: print("Clicked!"))
 btn.place(relx=0.5, rely=0.7, anchor="center")
 
 root.mainloop()
