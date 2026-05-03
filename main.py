dd931545f41476ca291295d41bd0f7d0f9510541
Dean-droid-alt <23023@students.mrgs.school.nz>
2026-05-04 10:50:10 +1200

    Continuing trial 2

--- main.py
+++ main.py
@@ -13,4 +13,14 @@ play_button = tk.PhotoImage(file="Image_Gallery/play_button.png")
 btn = tk.Button(root, image=play_button, cursor="hand2", command=lambda: print("Clicked!"))
 btn.place(relx=0.5, rely=0.7, anchor="center")
 
+def on_enter(event):
+    btn.config(bg = "#000000")
+
+def on_leave(event):
+    btn.config(bg = "#d8d3c9")
+
+btn.bind("<Enter>", on_enter)
+btn.bind("<Leave>", on_leave)
+
+
 root.mainloop()
