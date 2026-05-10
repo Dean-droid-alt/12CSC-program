aa212d43c2e376185ed73c5635dcc63980420710
Dean-droid-alt <23023@students.mrgs.school.nz>
2026-05-11 10:50:03 +1200

    Continuing trial 2

--- main.py
+++ main.py
@@ -9,38 +9,6 @@ label = tk.Label(root, image=photo)
 label.image = photo
 label.pack()
 
-play_button = tk.PhotoImage(file="Image_Gallery/Play_button.png")
-btn = tk.Button(root, image=play_button, cursor="hand2", command=lambda: label.pack_forget(), text="Hide Text")
-btn.place(relx=0.5, rely=0.77, anchor="center")
-
-class Quiz:
-    def __init__(self,parent):
-        Username = name_entry.get()
-        if Username.isdigit():
-            self.outcome_label.config(text="Cannot have numbers in your name, try again", fg="red")
-        elif Username.strip() == "":
-            self.outcome_label.config(text="Please enter a name, try again", fg="red")
-        elif any(char in "~!@#$%^&*()_+{}|:<>?*-=[]\\;',." for char in Username):
-            self.outcome_label.config(text="Cannot have special characters, try again", fg="red")
-        else:
-            root.after(1750, open_new_window)
-
-name_entry = tk.Entry(root, width=25, bd=2.5, font= ("Arial", 20))
-label = tk.Label(root, text=f"Welcome to the quiz {name_entry}",fg="green")
-label.place(relx=0.5, rely=0.53, anchor="center")
-name_entry.bind('<Return>', lambda event:name_entry.delete(0, tk.END))
-name_entry.place(relx=0.5, rely=0.48, anchor="center")
-
-
-
-def on_enter(event):
-    btn.config(bg = "#000000")
-
-def on_leave(event):
-    btn.config(bg = "#d8d3c9")
-
-btn.bind("<Enter>", on_enter)
-btn.bind("<Leave>", on_leave)
 
 def open_new_window():
     new_window = tk.Toplevel(root)
@@ -52,5 +20,33 @@ def open_new_window():
     image_label.pack(pady=40)
     root.withdraw()
 
+play_button = tk.PhotoImage(file="Image_Gallery/Play_button.png")
+btn = tk.Button(root, image=play_button, cursor="hand2", command=lambda: label.pack_forget(), text="Hide Text")
+btn.place(relx=0.5, rely=0.77, anchor="center")
+
+name_entry = tk.Entry(root, width=25, bd=2.5, font= ("Arial", 20))
+name_label = tk.Label(root, text=f"Welcome to the quiz,{name_entry}",fg="green")
+name_label.place(relx=0.5, rely=0.53, anchor="center")
+name_entry.bind('<Enter>')
+name_entry.place(relx=0.5, rely=0.48, anchor="center")
+
+if name_entry.isdigit():
+    self.outcome_label= tk.Label(text="Cannot have numbers in your name, try again", fg="red")
+    self.outcome_label.pack()
+elif Username.strip() == "":
+    self.outcome_label.config(text="Please enter a name, try again", fg="red")
+elif any(char in "~!@#$%^&*()_+{}|:<>?*-=[]\\;',." for char in Username):
+    self.outcome_label.config(text="Cannot have special characters, try again", fg="red")
+else:
+    root.after(1750, open_new_window)
+
+def on_enter(enter):
+    btn.config(bg = "#000000")
+
+def on_leave(leave):
+    btn.config(bg = "#d8d3c9")
+
+btn.bind("<Enter>", on_enter)
+btn.bind("<Leave>", on_leave)
 
 root.mainloop()
