084a4dcc9c52eedbc25a8c50ed92de56ae3ed102
Dean-droid-alt <23023@students.mrgs.school.nz>
2026-05-26 09:54:33 +1200

    Adding dictionary of questions and answers

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tkinter as tk
-from PIL import Image, ImageTk
+import tkinter as tk #Tells python to load the tkinter module
+from PIL import Image, ImageTk #Makes it so that I can use Image and ImageTK modules from the pillow library, this allows images to be processed
 
 root = tk.Tk()
-root.title("Home page")
+root.title("Home page") #Gives the title of my first window
 
 def home_page():
     image = Image.open("Image_Gallery/Homepage.png") #Opens image from image gallery folder
@@ -15,20 +15,20 @@ def home_page():
     name_entry.bind('<Enter>')
     name_entry.place(relx=0.5, rely=0.48, anchor="center") #Aligns the name entry box to the centre of the screen and moves it to a suitable position
 
-    outcome_label = tk.Label(root, text="Please enter your name", font=("Arial",9), bg="white")
-    outcome_label.place(relx=0.5, rely=0.53, anchor="center")
+    outcome_label = tk.Label(root, text="Please enter your name", font=("Arial",9), bg="white") #Creates a label with a specific message, font, font size and background
+    outcome_label.place(relx=0.5, rely=0.53, anchor="center") #Aligns the name entry box to the centre of the screen and moves it to a suitable position
 
     def name_checker():
-        name= name_entry.get()
+        name = name_entry.get() #Retrieves the text the user has entered
         if any(char.isdigit() for char in name): #Checks whether users name has any numbers in it
-            outcome_label.config(text="Cannot have numbers in your name, try again", fg="red") #Does not let the user move on to the next page and changes the text into the error message
+            outcome_label.config(text="Cannot have numbers in your name, try again", fg="red") #Does not let the user move on to the next page and changes the text in outcome_label into the error message in red
         elif name.strip() == "": #Checks whether user has entered anything into the box
-            outcome_label.config(text="Please enter a name, try again", fg="red") #Does not let the user move on to the next page and changes the text into the error message
+            outcome_label.config(text="Please enter a name, try again", fg="red") #Does not let the user move on to the next page and changes the text in outcome_label into the error message in red
         elif any(char in "~!@#$%^&*()_+{}|:<>?*-=[]\\;',." for char in name): #Checks whether users name has any symbols in it
-            outcome_label.config(text="Cannot have special characters, try again", fg="red") #Does not let the user move on to the next page and changes the text into the error message
-        else:
-            outcome_label.config(text= "Success!", fg="green")
-            root.after(1750, open_new_window)
+            outcome_label.config(text="Cannot have special characters, try again", fg="red") #Does not let the user move on to the next page and changes the text in outcome_label into the error message in red
+        else: #If there is no problem with the users name
+            outcome_label.config(text= "Success!", fg="green") #Changes the text in outcome_label into the success message in green
+            root.after(1750, open_new_window) #Lets the user move on to the next page using the open_new_window command and makes it happen after a short period of time
 
     play_button = tk.PhotoImage(file="Image_Gallery/Play_button.png")
     btn = tk.Button(root, image=play_button, cursor="hand2", command=name_checker)
@@ -56,7 +56,7 @@ Questions_answers = [
     {"Questions": "What country is this?", "Options": ["Sweden", "Norway", "Denmark", "England"], "Answer": "Sweden", "Background": "Image_Gallery/Q7.png"},
     {"Questions": "What is this famous landmark?", "Options": ["Neuschwanstein Castle", "Mont-Saint-Michel", "Potala Palace", "Petra"], "Answer": "Mont-Saint-Michel", "Background": "Image_Gallery/Q8.png"},
     {"Questions": "_______ is the country with the highest population", "Options": ["USA","Russia","China","India"], "Answer": "India", "Background": "Image_Gallery/Q9.png"},
-    {"Questions": "Which is the largest ocean on Earth?", "Options": ["Atlantic","Indian","Pacific","Arctic"], "Answer": "Pacific", "Background": "Image_Gallery/Q10.png"},
+    {"Questions": "What is the largest ocean on Earth?", "Options": ["Atlantic","Indian","Pacific","Arctic"], "Answer": "Pacific", "Background": "Image_Gallery/Q10.png"},
 ]
 
 def open_new_window():
@@ -98,7 +98,7 @@ def open_new_window():
     btn.place(relx=0.05, rely=0.09, anchor="center")
 
     Next_button = tk.PhotoImage(file="Image_Gallery/Next_button.png")
-    btn = tk.Button(new_window, image=Next_button, cursor="hand2")
+    btn = tk.Button(new_window, image=Next_button, cursor="hand2", bg = "#aea498", activebackground="#aea498", highlightthickness=0)
     btn.image = Next_button
     btn.place(relx=0.80, rely=0.845, anchor="center")
 
