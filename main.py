253e653bebd948c46d0b579b54945f2aa4d89611
Dean-droid-alt <23023@students.mrgs.school.nz>
2026-05-29 12:31:45 +1200

    Trying to display my questions and answers in my quiz

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ def home_page(): #Creates a def function for all elements in the home page
     label.pack()  #Makes the image for the homepage appear
 
     name_entry = tk.Entry(root, width=25, bd=2.5, font= ("Arial", 20)) #Creates the name entry box with specific width, length and font
-    name_entry.bind('<Return>')
+    name_entry.bind('<Return>', lambda event: name_checker()) #Makes it so that you can enter your name with the enter button
     name_entry.place(relx=0.5, rely=0.48, anchor="center") #Aligns the name entry box to the centre of the screen and moves it to a suitable position
 
     outcome_label = tk.Label(root, text="Please enter your name", font=("Arial",9), bg="white") #Creates a label with a specific message, font, font size and background
     outcome_label.place(relx=0.5, rely=0.53, anchor="center") #Aligns the name entry box to the centre of the screen and moves it to a suitable position
 
-    def name_checker(): #Creates a def function for
+    def name_checker(): #Creates a def function for the name checker part
         name = name_entry.get() #Retrieves the text the user has entered
         if any(char.isdigit() for char in name): #Checks whether users name has any numbers in it
             outcome_label.config(text="Cannot have numbers in your name, try again", fg="red") #Does not let the user move on to the next page and changes the text in outcome_label into the error message in red
@@ -41,12 +41,12 @@ def home_page(): #Creates a def function for all elements in the home page
     def on_leave(leave): #Creates a variable for what happens when the mouse is off the button
         btn.config(bg = "#d8d3c9") #Makes the background of the button grayish orange
 
-    btn.bind("<Enter>", on_enter) #Connects the mouse hovering over the button to the change in background of the button
-    btn.bind("<Leave>", on_leave)
+    btn.bind("<Enter>", on_enter) #Makes it so that when the mouse is hovering over the button it changes the outline of the button
+    btn.bind("<Leave>", on_leave) #Makes it so that when the mouse is not hovering over the button it goes back to normal
 
-home_page()
+home_page() #Runs the code in the home_page def function
 
-Questions_answers = [
+Questions_answers = [ #Questions and answers for my quiz
     {"Questions": "What is the capital of Canada?", "Options": ["Montreal","Toronto","Vancouver","Ottawa"], "Answer": "Ottawa", "Background": "Image_Gallery/Q1.png"},
     {"Questions": "What country is this?", "Options": ["India","China","Japan","Indonesia"], "Answer": "India", "Background": "Image_Gallery/Q2.png"},
     {"Questions": "What is this famous landmark?", "Options": ["Statue of liberty","Stonehenge","Christ The Redeemer", "Colosseum"], "Answer": "Christ the redeemer", "Background": "Image_Gallery/Q3.png"},
@@ -59,38 +59,40 @@ Questions_answers = [
     {"Questions": "What is the largest ocean on Earth?", "Options": ["Atlantic","Indian","Pacific","Arctic"], "Answer": "Pacific", "Background": "Image_Gallery/Q10.png"},
 ]
 
-def open_new_window():
-    new_window = tk.Toplevel(root)
-    new_window.title("Questions page")
-    next_image = Image.open("Image_Gallery/Question_page.png")
-    next_photo = ImageTk.PhotoImage(next_image)
-    image_label = tk.Label(new_window, image=next_photo)
-    image_label.image = next_photo
-    image_label.pack()
+def open_new_window(): #Creates a def function for the questions page
+    new_window = tk.Toplevel(root) #Creates a secondary window
+    new_window.title("Questions page") #Creates the title of the second window
+    next_image = Image.open("Image_Gallery/Question_page.png") #Opens the image from image gallery folder
+    next_photo = ImageTk.PhotoImage(next_image) #Converts an image to a tkinter compatible format
+    image_label = tk.Label(new_window, image=next_photo) #Creates a widget that displays an image instead of text
+    image_label.image = next_photo #Attaches the label to the image so that is not deleted by pythons memory cleanup
+    image_label.pack() #Makes the image for the homepage appear
+
+    current_question = Questions_answers[0]
 
     def check_answer(selected_answer):
-        if selected_answer == ["Answer"]:
+        if selected_answer == current_question["Answer"]:
             print("Correct")
         else:
             print("Wrong")
 
     Answer1_button = tk.PhotoImage(file="Image_Gallery/Answer1.png")
-    btn1 = tk.Button(new_window, image=Answer1_button, cursor="hand2", text=(["Options"][1]), command=lambda: check_answer(["Options"][1]))
+    btn1 = tk.Button(new_window, image=Answer1_button, cursor="hand2", text= current_question["Options"][0], compound="center", command=lambda: check_answer(current_question["Options"][0]))
     btn1.image = Answer1_button
     btn1.place(relx=0.38, rely=0.75, anchor="center")
 
     Answer2_button = tk.PhotoImage(file="Image_Gallery/Answer2.png")
-    btn2 = tk.Button(new_window, image=Answer2_button, cursor="hand2")
+    btn2 = tk.Button(new_window, image=Answer2_button, cursor="hand2", text= current_question["Options"][1], compound="center", command=lambda: check_answer(current_question["Options"][1]))
     btn2.image = Answer2_button
     btn2.place(relx=0.625, rely=0.75, anchor="center")
 
     Answer3_button = tk.PhotoImage(file="Image_Gallery/Answer3.png")
-    btn3 = tk.Button(new_window, image=Answer3_button, cursor="hand2")
+    btn3 = tk.Button(new_window, image=Answer3_button, cursor="hand2", text= current_question["Options"][2], compound="center", command=lambda: check_answer(current_question["Options"][2]))
     btn3.image = Answer3_button
     btn3.place(relx=0.38, rely=0.92, anchor="center")
 
     Answer4_button = tk.PhotoImage(file="Image_Gallery/Answer4.png")
-    btn4 = tk.Button(new_window, image=Answer4_button, cursor="hand2")
+    btn4 = tk.Button(new_window, image=Answer4_button, cursor="hand2", text= current_question["Options"][3], compound="center", command=lambda: check_answer(current_question["Options"][3]))
     btn4.image = Answer4_button
     btn4.place(relx=0.625, rely=0.92, anchor="center")
 
@@ -107,6 +109,8 @@ def open_new_window():
     btn = tk.Button(new_window, image=Next_button, cursor="hand2", bg = "#aea498", activebackground="#aea498", highlightthickness=0)
     btn.image = Next_button
     btn.place(relx=0.80, rely=0.845, anchor="center")
+
+    new_window.protocol("WM_DELETE_WINDOW", exit_to_home)
 
     root.withdraw()
 
